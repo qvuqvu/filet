64ce951cc46b16b33230136892d21534c618c4b2
Quang Vu <ngqangvvu@gmail.com>
2023-07-30 18:28:01 +0700

    Establish pages and folder structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const DashboadPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Button variant="destructive" size="default">
+        Hi
+      </Button>
+    </div>
+  );
+};
+
+export default DashboadPage;
